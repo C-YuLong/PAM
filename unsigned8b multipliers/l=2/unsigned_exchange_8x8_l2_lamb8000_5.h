0ed36f1c9e0490ebe1bfb967bd70bfc6c07d2228
C-YuLong <77819878+C-YuLong@users.noreply.github.com>
2023-07-14 13:03:07 +0800

    Add files via upload

---
+++ unsigned8b multipliers/l=2/unsigned_exchange_8x8_l2_lamb8000_5.h
@@ -0,0 +1,28 @@
+// terms: 3
+// fval:  31707.25
+
+#include <bitset>
+#include <cmath>
+
+using namespace std;
+
+uint16_t unsigned_exchange_8x8_l2_lamb8000_5(uint16_t i, uint16_t j) {
+
+bitset<8> x = i;
+
+bitset<8> part1 = j*x[0];
+bitset<8> part2 = j*x[1];
+bitset<8> part3 = j*x[2];
+bitset<8> part4 = j*x[3];
+bitset<8> part5 = j*x[4];
+bitset<8> part6 = j*x[5];
+bitset<8> part7 = j*x[6];
+bitset<8> part8 = j*x[7];
+
+uint16_t tmp_z =  j*x[2]*pow(2, 2) + j*x[3]*pow(2, 3) + j*x[4]*pow(2, 4) + j*x[5]*pow(2, 5) + j*x[6]*pow(2, 6) + j*x[7]*pow(2, 7);
+
+uint16_t z = tmp_z + (part1[7]&part2[6])*pow(2, 8) + (part1[7]^part2[6])*pow(2, 7) + (part2[7]*pow(2, 8));
+
+return z;
+
+}
